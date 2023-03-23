a52c2db5fce98507658a859fa222aea48c2a0f93
Mayakuai <joshuasstuart@gmail.com>
2023-03-23 01:34:29 -0400

    changed root file to index

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
- 
- 
+
+
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 const { restart } = require("nodemon");
- 
+
 mailchimp.setConfig({
   apiKey: 'c60ac942bad8b26c8a05e7ec3104ad00-us10',
   server: 'us10',
 });
- 
- 
+
+
 const app = express();
- 
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/signup.html")
   });
- 
+
 app.post("/", function(req, res) {
- 
+
   var firstName = req.body.fName
   var lastName = req.body.lName
   var email =req.body.email
- 
-  
+
+
   const run = async () => {
     const response = await mailchimp.lists.batchListMembers("8cb2405f52", {
       members: [{
@@ -37,7 +37,7 @@ app.post("/", function(req, res) {
           LNAME: lastName
         }
       }],
-    });     
+    });
   };
   run().then(
     (f) => {
@@ -49,20 +49,20 @@ app.post("/", function(req, res) {
       res.sendFile(__dirname + "/failure.html");
     });
 });
- 
+
 app.post('/failure', (req, res) => {
     res.redirect('/');
 });
- 
- 
+
+
 app.listen(process.env.PORT || 3000, function() {
-    console.log("server started")    
+    console.log("server started")
 });
- 
 
 
 
-// api key for Mailchimp 
+
+// api key for Mailchimp
 // c60ac942bad8b26c8a05e7ec3104ad00-us10
 
 // list ID for mailchimp
